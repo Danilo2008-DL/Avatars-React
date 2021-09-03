eea9e66b9dca88ce159c80b060fe1913b4dcfebe
Danilo Medeiros <63961886+Danilo2008-DL@users.noreply.github.com>
2021-09-02 22:55:31 -0300

    Add files via upload

---
+++ app-entertainment/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import avatar1 from './assets/avatars/avatar1.png';
+import avatar2 from './assets/avatars/avatar2.png';
+import avatar3 from './assets/avatars/avatar3.png';
+import avatar4 from './assets/avatars/avatar4.png';
+import avatar5 from './assets/avatars/avatar5.png';
+
+
+
+
+export function App() {
+  return (
+    <div className="App">
+      <div class="avatars">
+      <img id='avatar1' src={avatar1}></img>
+      <img id='avatar2' src={avatar2}></img>
+      <img id='avatar3' src={avatar3}></img>
+      <img id='avatar4' src={avatar4}></img>
+      <img id='avatar5' src={avatar5}></img> 
+    </div>
+    
+      
+    
+
+        
+          
+      
+ 
+    </div>
+  );
+}
+
+export default App;
